f1a91166022615157d19ff38eb7c42d3b591b8bd
Ariel Blasi <exorcismo@gmail.com>
2017-09-12 19:21:12 -0300

    Minor edits

--- models/user.js
+++ models/user.js
@@ -1,13 +1,15 @@
-let mongoose = require("mongoose")
-// USER SQUEMA - email, name
+let mongoose = require("mongoose");
+// USER SCUEMA - email, name
 let userSchema = new mongoose.Schema({
-	email: String,
-	name: String,
-	// array of posts, taking from the post schema name, which is postSchema from above
-	posts: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Post"
-	}]
-})
+  email: String,
+  name: String,
+  // array of posts, taking from the post schema name, which is postSchema from above
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post"
+    }
+  ]
+});
 // USER MODEL - it's covention to match the name of the model, with capital first
-module.exports = mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema);
--- references.js
+++ references.js
@@ -1,36 +1,45 @@
-let mongoose = require("mongoose")
-//mongoose.connect("mongodb://ref:ref@ds028310.mlab.com:28310/references")
+let mongoose = require("mongoose");
 // Using `mongoose.connect`...
-const promise = mongoose.connect("mongodb://ref:ref@ds028310.mlab.com:28310/references", {
-  useMongoClient: true,
-  /* other options */
-});
+const promise = mongoose.connect(
+  "mongodb://ref:ref@ds028310.mlab.com:28310/references",
+  {
+    useMongoClient: true
+    /* other options */
+  }
+);
 
-const Post = require("./models/post")
-const User = require("./models/user")
+const Post = require("./models/post");
+const User = require("./models/user");
 
 // // create User, is new user + save user
 // User.create({
 // 	email: "bobby@justbob.com",
 // 	name: "Bobby Blob"
 // })
-// I don't think this is very conventional, best to research on Scotch or official docs  to know how
+// I don't think this is very conventional, best to research on Scotch or official docs to know how
 // to handle all this messy mess:
-Post.create({
-	title: "But family will remain",
-	content: "And that's the foundation you can count on"
-}, function(err, post){
-	User.findOne({email: "bobby@justbob.com"}, function(err,foundUser){
-		if(err){console.log(err)}
-		else{
-			foundUser.posts.push(post)
-			foundUser.save(function(err,data){
-				if(err){console.log(err)}
-				else{console.log(data)}
-			})
-		}
-	})
-})
+Post.create(
+  {
+    title: "But family will remain",
+    content: "And that's the foundation you can count on"
+  },
+  function(err, post) {
+    User.findOne({ email: "bobby@justbob.com" }, function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        foundUser.posts.push(post);
+        foundUser.save(function(err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(data);
+          }
+        });
+      }
+    });
+  }
+);
 
 //find User and find and populate all posts from that user
 // User.findOne({email:"bobby@justbob.com"}).populate("posts").exec(function(err,user){
